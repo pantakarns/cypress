ab3b90baf6cd3614c58d92a68439c0a8794520dc
pantakarns <102522906+pantakarns@users.noreply.github.com>
2024-03-23 14:10:59 +0700

    Delete myHotel_demo directory

--- myHotel_demo/cypress/integration/testcypress/myHotel.feature
+++
@@ -1,4 +0,0 @@
-Feature: Todo Page
-
-  Scenario: Open my hotel page and input form
-    Given open todo page
--- myHotel_demo/cypress/keywords/features/TodoKeywords.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="cypress" />
-
-import { When, Then, And } from "cypress-cucumber-preprocessor/steps";
-
-Given("open todo page", () => {
-  cy.visit("https://panaryco.wixsite.com/myhotel");
-});
